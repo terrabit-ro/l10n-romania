41e44f1443a380d9cc363d6b388cf6927fccfcd7
Dorin Hongu <dhongu@gmail.com>
2026-06-28 09:23:31 +0300

    [IMP] l10n_ro_stock_account_notice: pre-commit stuff

--- l10n_ro_stock_account_notice/__manifest__.py
+++ l10n_ro_stock_account_notice/__manifest__.py
@@ -6,7 +6,7 @@
     "category": "Localization",
     "countries": ["ro"],
     "summary": "Romania - Stock Accounting Notice",
-    "author": "NextERP Romania," "Dorin Hongu," "Odoo Community Association (OCA)",
+    "author": "NextERP Romania,Dorin Hongu,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-romania",
     "depends": ["l10n_ro_stock_account"],
     "license": "AGPL-3",
